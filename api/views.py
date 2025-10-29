1d67276e9636dd970cd74edd5c1c01727522d887
Chamidu Dilshan <chamidudilshan0423@gmail.com>
2025-10-30 00:11:36 +0530

    Add REST framework to settings and configure API URLs

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from rest_framework.response import responses
+from rest_framework.response import Response
 
 @api_view(["GET"])
 def hello_world(request):
